ac76aa97b03790b69c3e39de8583980f85fa8a21
wxu <winnielandau@gmail.com>
2015-04-01 13:39:28 -0700

    wxu add comments and incorporate minor changes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :discussion
-
   validates :body, presence: true
 
   # this will give you the post of the discussion that this comment 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 20150401175705) do
+=======
+ActiveRecord::Schema.define(version: 20150401180046) do
+>>>>>>> Stashed changes
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
